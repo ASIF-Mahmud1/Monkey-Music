64df8b9fa6dccf669081e182d2153f1de099d411
Asif <1421015@iub.edu.bd>
2022-02-25 15:16:03 +0600

    [init]

---
+++ App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/menu/container/Home';
+import User from './src/menu/container/User'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { NavigationContainer } from '@react-navigation/native';
+console.disableYellowBox = true;
+
+
+const Stack = createNativeStackNavigator();
+export default function Authorised(){
+    return(
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name='Home' component={Home}/>
+              <Stack.Screen name='User' component={User} /> 
+          </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+//  options={{headerShown:false}}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
